86f7bb5b414c24dc5bfb4f077cef1651d4d90a8b
Kim Seongjin <darkmoon625@naver.com>
2023-10-13 16:18:24 +0900

    # 2133 update

--- Oct_week2/2133.py
+++ Oct_week2/2133.py
@@ -6,10 +6,7 @@ dp = [0] * 31
 
 dp[2] = 3
 
-for i in range(4,n+1) :
-    if i%2 == 0 :
+for i in range(4, n+1, 2) :
         dp[i] = dp[i-2] * 3 + sum(dp[:i-2]) * 2 + 2
-    else :
-        dp[i] = 0
 
 print(dp[n])
