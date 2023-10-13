1641cae42824003359fbd88755232a403a57f009
Kim Seongjin <darkmoon625@naver.com>
2023-10-13 16:06:49 +0900

    # 2133 done

--- Oct_week2/2133.py
+++ Oct_week2/2133.py
@@ -2,9 +2,14 @@ import sys
 
 
 n = int(sys.stdin.readline())
+dp = [0] * 31
 
-dp = [0] * (n+1)
-
-dp[1] = 2
 dp[2] = 3
 
+for i in range(4,n+1) :
+    if i%2 == 0 :
+        dp[i] = dp[i-2] * 3 + sum(dp[:i-2]) * 2 + 2
+    else :
+        dp[i] = 0
+
+print(dp[n])
