7de4619b7422ddeebb24c25b2fd908c4d8d8c671
LTT <68013705+ksj000625@users.noreply.github.com>
2025-03-29 15:47:57 +0900

    [Silver IV] Title: 괄호, Time: 192 ms, Memory: 17920 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Stack;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int T = scan.nextInt();
+		
+		for(int i = 0; i < T; i++) {
+			System.out.println(solve(scan.next()));
+		}
+	}
+
+	public static String solve(String s) {
+		Stack<Character> stack = new Stack<>();
+ 
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+ 
+			if (c == '(') {
+				stack.push(c);
+			}
+			else if (stack.empty()) {
+				return "NO";
+			}
+			else {
+				stack.pop();
+			}
+		}
+ 
+		if (stack.empty()) {
+			return "YES";
+		} 
+		else {
+			return "NO";
+		}
+	}
+}
